6f39c3f51a665af8c9bb6266acb01c787ca97c26
luocy <1475653689@qq.com>
2017-12-23 18:35:29 +0800

    第三次提交

---
+++ HadoopClient/src/main/java/com/lcydream/open/project/hdfs/HdfsClient.java
@@ -0,0 +1,11 @@
+package com.lcydream.open.project.hdfs;/**
+ * @author LuoChunYun E-mail:1475653689@qq.com
+ * @version 创建时间：${date} ${time}
+ */
+
+/**
+ * @author Luo Chun Yun
+ * @date 2017/12/2318:25
+ */
+public class HdfsClient {
+}
---
+++ HadoopClient/src/test/java/com/lcydream/open/project/hdfs/HdfsClientDemo.java
@@ -0,0 +1,37 @@
+package com.lcydream.open.project.hdfs;
+
+/**
+ * HdfsClientDemo
+ * @author Luo Chun Yun
+ * @date 2017/12/23 18:17:01
+ */
+public class HdfsClientDemo {
+
+
+    public void init(){
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
